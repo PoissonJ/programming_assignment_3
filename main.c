d7c000b766e26011fd8ecece36013704137feb12
Derek Schanze <dschanze@Dereks-MacBook-Air.local>
2015-11-23 14:24:29 -0500

    Added student structure

--- main.c
+++ main.c
@@ -3,5 +3,16 @@
 
 #include "functions.h"
 
-// Main
-// bleh bleh 
+// Student Struct
+struct Student{
+    char first_name[50]; //change to string
+    char last_name[50]; //change to string
+    int id;
+    int math_grade;
+    int music_grade;
+    int pe_grade;
+};
+
+int main(void) {
+    return 0;
+}
