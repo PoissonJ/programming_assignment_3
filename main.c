e60762f645870a8c3bfd2bcd8416489ad6ae1181
Jonathan <poissonj@ufl.edu>
2015-11-28 11:16:14 -0500

    Removed functions.h include statement and extra whitespace from main.c

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#include "functions.h"
-
 // Student Struct
 typedef struct student{
     char first_name[50]; //change to string
@@ -29,12 +27,12 @@ void print_data(int stud_id);
 void search();
 
 int main(void) {
-    
+
     char choice;
-    
+
     for(;;){
     scanf("%1c", &choice);
-    
+
     switch(choice){
         case 'A':
             i_max = i;
@@ -69,7 +67,7 @@ int main(void) {
             search();
             break;*/
         }
-    
+
         /*if(choice == 'A'){
          scanf(" %s %s %d %d %d %d", student_array[i].first_name, student_array[i].last_name, &student_array[i].id, &student_array[i].math_grade, &student_array[i].music_grade, &student_array[i].pe_grade);
          printf("\n%s %s %d %d %d %d\n", student_array[i].first_name, student_array[i].last_name, student_array[i].id, student_array[i].math_grade, student_array[i].music_grade, student_array[i].pe_grade);
@@ -77,21 +75,21 @@ int main(void) {
     }
 
 quit: ;
-    
+
     return 0;
 }
 
 void read_data(int i_max)
 {
     //int i = 0;
-    
+
     scanf(" %s %s %d %f %f %f", student_array[i_max].first_name, student_array[i_max].last_name, &student_array[i_max].id, &student_array[i_max].math_grade, &student_array[i_max].music_grade, &student_array[i_max].pe_grade);
 }
 
 void list_data(int i_max)
 {
     //int i = 0;
-    
+
     for(i = 0; i <= i_max; i++){
         printf("%s %s [%d]:\n", student_array[i].first_name, student_array[i].last_name, student_array[i].id);
         printf("        MATH:   %4.2f\n", student_array[i].math_grade);
@@ -99,7 +97,7 @@ void list_data(int i_max)
         printf("        PE:     %4.2f\n", student_array[i].pe_grade);
         printf("                %4.2f\n", (student_array[i].math_grade + student_array[i].music_grade + student_array[i].pe_grade)/3.0f);
     }
-    
-    
+
+
     /*printf("\n%s %s %d %d %d %d\n", student_array[i].first_name, student_array[i].last_name, student_array[i].id, student_array[i].math_grade, student_array[i].music_grade, student_array[i].pe_grade);*/
 }
